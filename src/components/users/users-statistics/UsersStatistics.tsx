f6fd6d2a8d6ba2f12ad5dc18c67e595b09d9ba8f
Siim Kuusküll <siim_k@outlook.com>
2024-11-25 01:25:36 +0200

    user-statistics improvements: user balances added

--- src/components/users/users-statistics/UsersStatistics.tsx
+++ src/components/users/users-statistics/UsersStatistics.tsx
@@ -1,69 +1,39 @@
 import { Table } from 'antd';
+import { useEffect, useState } from 'react';
 
 export default function UsersStatistics() {
-    return <Table title={() => 'User Statistics'} columns={columns} dataSource={data} />;
+    const [userStatistics, setUserStatistics] = useState([]);
+    useEffect(() => {
+        getUserStatistics();
+    }, []);
+    async function getUserStatistics() {
+        const response = await fetch('http://172.22.31.60:3000/users/user-statistics', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const x = await response.json();
+        setUserStatistics(x);
+    }
+    return <Table title={() => 'User Statistics'} columns={columns} dataSource={userStatistics} />;
 }
 
 const columns = [
     {
         title: 'Id',
-        dataIndex: 'id',
+        dataIndex: 'user_id',
     },
     {
         title: 'Name',
-        dataIndex: 'name',
+        dataIndex: 'user_name',
     },
     {
         title: 'Balance',
-        dataIndex: 'balance',
+        dataIndex: 'amount',
     },
     {
         title: 'Profit',
         dataIndex: 'profit',
-    },
-];
-
-const data = [
-    {
-        key: '1',
-        id: 1357357438171388,
-        name: 'jumpermaku',
-        balance: 0,
-        profit: -10,
-    },
-    {
-        key: '2',
-        id: 91485904851,
-        name: 'ˇMihkel',
-        balance: 32,
-        profit: 30,
-    },
-    {
-        key: '3',
-        id: 5687932748,
-        name: 'Estmaster',
-        balance: 32,
-        profit: 30,
-    },
-    {
-        key: '4',
-        id: 126787433,
-        name: 'Pulk',
-        balance: 32,
-        profit: 30,
-    },
-    {
-        key: '5',
-        id: 10100007,
-        name: 'Steve',
-        balance: 32,
-        profit: 30,
-    },
-    {
-        key: '6',
-        id: 1256767868654,
-        name: 'Peeter',
-        balance: 32,
-        profit: 30,
     },
 ];
